409d128e63e0bccdff25b464037365bc7f0d4fe2
Ashton B <ashtonbrugh@gmail.com>
2022-05-18 09:58:18 -0400

    ignoring settings

--- plantindex/plants/models.py
+++ plantindex/plants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    genus = models.CharField(max_length=100, blank=True)
+    species= models.CharField(max_length=100, blank=True)
+    
+
